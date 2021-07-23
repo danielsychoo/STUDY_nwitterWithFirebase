6fef974d625b43d9e52e8c31043c872438b799f6
sungyeopTW <danielchoo@twinny.ai>
2021-07-23 17:11:44 +0900

    Update init firebase logic

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { FIREBASE_AUTH } from 'config/firebase';
 import { Switch, Route } from 'react-router-dom';
 import { Navigation } from 'components';
@@ -7,21 +7,31 @@ import { useToggle } from 'hooks';
 
 function App() {
   const { isOn, setIsOn } = useToggle();
+  const [test, setTest] = useState(false);
 
   useEffect(() => {
-    FIREBASE_AUTH.onAuthStateChanged(auth =>
-      auth ? setIsOn(true) : setIsOn(false),
-    );
+    FIREBASE_AUTH.onAuthStateChanged(auth => {
+      {
+        auth ? setIsOn(true) : setIsOn(false);
+        setTest(true);
+      }
+    });
   }, []);
 
   return (
     <div>
-      {isOn && <Navigation />}
-      <Switch>
-        <Route exact path="/" render={() => (isOn ? <Home /> : <Auth />)} />
-        <Route path="/myProfile" component={Profile} />
-        <Route path="/editProfile" component={EditProfile} />
-      </Switch>
+      {test ? (
+        <>
+          {isOn && <Navigation />}
+          <Switch>
+            <Route exact path="/" render={() => (isOn ? <Home /> : <Auth />)} />
+            <Route path="/myProfile" component={Profile} />
+            <Route path="/editProfile" component={EditProfile} />
+          </Switch>
+        </>
+      ) : (
+        <div>false</div>
+      )}
     </div>
   );
 }
